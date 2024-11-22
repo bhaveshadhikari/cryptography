ce3ae7f36d3c5c58a67a2ff8bf46ac99371eed37
bhavesh_adhikari <108828490+bhaveshadhikari@users.noreply.github.com>
2024-11-22 23:21:43 +0545

    added decryption

--- 1.ceasercipher.c
+++ 1.ceasercipher.c
@@ -1,23 +1,30 @@
-
 #include <stdio.h>
 #define KEY 3
-char plainText[]="THISTHEPLAINTEXT";
+char plainText[]="THISISPLAINTEXT";
 int len = sizeof(plainText)/sizeof(plainText[0])-1;
 char cipherText[sizeof(plainText)/sizeof(plainText[0])];
+char decryText[sizeof(plainText)/sizeof(plainText[0])];
 
-void ceaserCipher(){
+void ceaserCipherEncry(){
     for(int i = 0; i < len; i++){
         cipherText[i] = ((plainText[i]+KEY)-'A')%26+'A';
     }
     
+}
+void ceaserCipherDecry() {
+    for (int i = 0; i < len; i++) {
+        decryText[i] = ((cipherText[i] - KEY - 'A' + 26) % 26) + 'A';
+    }
 }
 int main() {
     
     printf("simulation of hill cipher\n");
     printf("key(shift) : %d\n",KEY);
     printf("plain  text: %s\n",plainText);
-    ceaserCipher();
+    ceaserCipherEncry();
     printf("cipher text: %s\n",cipherText);
+    ceaserCipherDecry();
+    printf("decrypted text: %s\n",decryText);
 
     return 0;
 }
@@ -25,6 +32,7 @@ int main() {
 /*
 simulation of hill cipher
 key(shift) : 3
-plain  text: THISTHEPLAINTEXT
-cipher text: WKLVWKHSODLQWHAW
+plain  text: THISISPLAINTEXT
+cipher text: WKLVLVSODLQWHAW
+decrypted text: THISISPLAINTEXT
 */
